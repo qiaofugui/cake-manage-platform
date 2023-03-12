d5e8e82ceb0a04ae8279afe3cf37be6c73aedf01
qiaofugui <903111082@qq.com>
2023-03-12 17:40:52 +0800

    全局配置 umirc 进行拆分

--- config/config.js
+++ config/config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'umi';
 
+import routes from './routes';
+
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  // 配置式路由
+  routes: routes,
   fastRefresh: {},
+  // 配置开启 antd 组件库
+  antd: {
+    dark: false,
+  },
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import styles from './index.less';
+import { Button } from 'antd';
 
 export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <Button>11</Button>
     </div>
   );
 }
